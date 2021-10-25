d16ce373a687613c3c87b2265c777fa9943a082e
Haing1974 <43593345+Haing1974@users.noreply.github.com>
2021-10-25 12:59:09 +0700

    add footer

--- laundry-service/src/App.js
+++ laundry-service/src/App.js
@@ -1,11 +1,14 @@
 import Topbar from "./topbar/Topbar";
 import FrontPage from "./pages/front-page/FrontPage";
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
+import Footer from "./foooter/Footer";
 
 function App() {
   return (
     <>
       <Topbar/>
       <FrontPage/>
+      <Footer/>
     </>
   );
 }
